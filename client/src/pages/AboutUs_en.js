55d0e16ddf052512d63014c1b771fb04a43669db
JC <juanky201271@gmail.com>
2021-02-27 17:57:49 -0700

    About us done! with the new design. Issues #5 and #4.

---
+++ client/src/pages/AboutUs_en.js
@@ -0,0 +1,20 @@
+const labels = {
+  who_we_are: 'Who we are',
+  who_we_are_text: 'It is a multidisciplinary digital platform made up of experts in various fields, among which artificial intelligence, fishing, IT and international trade stand out. This platform is distributed over a network with nodes established on four continents.',
+
+  what_do_we_do: 'What do we do',
+  what_do_we_do_text: 'Startup whose main objective is to facilitate the use of artificial intelligence tools in fishing in particular and in the agri-food sector in general.',
+
+  our_products: 'Our products',
+
+  obj_det_tool: 'Object detection tool',
+  obj_det_tool_text: 'Application to automatically identify different species of fish, quantify and measure them from video or photo images.',
+
+  blo_tool: 'Blockchain tool',
+  blo_tool_text: 'Blockchain application to establish the digital traceability of agri-food products.',
+
+  rec_tool: 'Recognition tool',
+  rec_tool_text: 'Fishing boat recognition application based on facial recognition systems from video or photo images.',
+};
+
+export { labels };
---
+++ client/src/pages/AboutUs_es.js
@@ -0,0 +1,20 @@
+const labels = {
+  who_we_are: 'Quienes somos',
+  who_we_are_text: 'Es una plataforma digital multidisciplinar constituida por expertos en diversos campos entre los que destacan la inteligencia artificial, pesca, Informática y comercio internacional. Dicha plataforma se distribuye en red con nodos establecidos en cuatro continentes.',
+
+  what_do_we_do: 'Qué hacemos',
+  what_do_we_do_text: 'Startup cuyo objetivo fundamental es facilitar el uso de herramientas de inteligencia artificial en la pesca en particular y en el sector agroalimentario en general.',
+
+  our_products: 'Nuestros productos',
+
+  obj_det_tool: 'Herramienta de detección de objetos',
+  obj_det_tool_text: 'Aplicación para identificar automáticamente diferentes especies de peces, cuantificarlas y medirlas a partir de imágenes de video o foto.',
+
+  blo_tool: 'Herramienta Blockchain',
+  blo_tool_text: 'Aplicación blockchain para establecer la trazabilidad digital de productos agroalimentarios.',
+
+  rec_tool: 'Herramienta de reconocimiento',
+  rec_tool_text: 'Aplicación de reconocimiento de barcos pesqueros basada en sistemas de reconocimiento facial a partir de imágenes de video o foto.',
+};
+
+export { labels };
