1ff2dcdbd0beea18168114e8c147499122cf0110
JC <juanky201271@gmail.com>
2021-03-10 16:08:05 -0700

    flask in different repo.

--- client/src/pages/SubmitFile.jsx
+++ client/src/pages/SubmitFile.jsx
@@ -246,7 +246,7 @@ class SubmitFile extends Component {
       const { uploadedFile, dir, model, width_cms, width_pxs_x_cm } = this.state
 
       if (dir !== null) {
-        await api.videoRoiCountFishAwsS3(process.env.REACT_APP_AWS_Uploaded_FIle_URL_Link + 'submits/' + dir + '/' + uploadedFile, model, width_cms, width_pxs_x_cm)
+        await api.videoRoiCountFishAwsS3(process.env.REACT_APP_AWS_Uploaded_FIle_URL_Link + 'submits/' + dir + '/' + uploadedFile, process.env.REACT_APP_AWS_Uploaded_FIle_URL_Link + 'submits/' + dir + '/' + model, width_cms, width_pxs_x_cm)
           .then(res => {
             this.setState({ total_fish: res.data.total_fish })
           })
@@ -261,7 +261,7 @@ class SubmitFile extends Component {
       const { uploadedFile, dir, model, width_cms, width_pxs_x_cm } = this.state
 
       if (dir !== null) {
-        await api.videoCountFishAwsS3(process.env.REACT_APP_AWS_Uploaded_FIle_URL_Link + 'submits/' + dir + '/' + uploadedFile, model, width_cms, width_pxs_x_cm)
+        await api.videoCountFishAwsS3(process.env.REACT_APP_AWS_Uploaded_FIle_URL_Link + 'submits/' + dir + '/' + uploadedFile, process.env.REACT_APP_AWS_Uploaded_FIle_URL_Link + 'submits/' + dir + '/' + model, width_cms, width_pxs_x_cm)
           .then(res => {
             this.setState({ total_fish: res.data.total_fish })
           })
@@ -276,7 +276,7 @@ class SubmitFile extends Component {
       const { uploadedFile, dir, model, width_cms, width_pxs_x_cm } = this.state
 
       if (dir !== null) {
-        await api.webcamCountFishAwsS3(process.env.REACT_APP_AWS_Uploaded_FIle_URL_Link + 'submits/' + dir + '/' + uploadedFile, model, width_cms, width_pxs_x_cm)
+        await api.webcamCountFishAwsS3(process.env.REACT_APP_AWS_Uploaded_FIle_URL_Link + 'submits/' + dir + '/' + uploadedFile, process.env.REACT_APP_AWS_Uploaded_FIle_URL_Link + 'submits/' + dir + '/' + model, width_cms, width_pxs_x_cm)
           .then(res => {
             this.setState({ total_fish: res.data.total_fish })
           })
@@ -291,7 +291,7 @@ class SubmitFile extends Component {
       const { uploadedFile, dir, model, width_cms, width_pxs_x_cm } = this.state
 
       if (dir !== null) {
-        await api.pictureCountFishAwsS3(process.env.REACT_APP_AWS_Uploaded_FIle_URL_Link + 'submits/' + dir + '/' + uploadedFile, model, width_cms, width_pxs_x_cm)
+        await api.pictureCountFishAwsS3(process.env.REACT_APP_AWS_Uploaded_FIle_URL_Link + 'submits/' + dir + '/' + uploadedFile, process.env.REACT_APP_AWS_Uploaded_FIle_URL_Link + 'submits/' + dir + '/' + model, width_cms, width_pxs_x_cm)
           .then(res => {
             this.setState({ total_fish: res.data.total_fish })
           })
