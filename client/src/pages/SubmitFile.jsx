f53e6a5ea050309d0a9e5ad663567a90da281243
JC <juanky201271@gmail.com>
2021-05-05 16:29:43 -0600

    if error, no files, it would nice if heroku rerun the process.

--- client/src/pages/SubmitFile.jsx
+++ client/src/pages/SubmitFile.jsx
@@ -82,8 +82,9 @@ class SubmitFile extends Component {
         if (cola.length > 0) {
           for (let i = 0; i < cola.length; i++) {
             const uploadedFileState = 'submits/' + cola[i].dir + '/' + cola[i].uploadedFile
-            if (params.uploadedFile === uploadedFileState && cola[i].log !== 'end' && cola[i].log !== 'ERROR') {
+            if (params.uploadedFile === uploadedFileState && cola[i].log !== 'end') {
               // si ya esta finalizado no hacer nada
+              // si ha dado error pero al final el proceso se lanza, recibir las notificaciones, ¿por qué no?
               if (cola[i].selectedFile.type.split('/')[0] === 'video') {
                 //video
                 if (params.action === 'start') {
